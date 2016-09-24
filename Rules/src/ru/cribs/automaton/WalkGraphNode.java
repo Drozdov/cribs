5cd4377cc1f298cd0e5e8961d3f538566461094e
Drozdov <Drozdov_VA@list.ru>
2016-09-24 15:51:37 +0300

    WalkGraphNnode implementation

--- Rules/src/ru/cribs/automaton/WalkGraphNode.java
+++ Rules/src/ru/cribs/automaton/WalkGraphNode.java
@@ -1,7 +1,10 @@
 package ru.cribs.automaton;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -11,29 +14,73 @@ public class WalkGraphNode {
 	public Map<RuleData, WalkGraphNode> edges = new HashMap<>();
 	public Set<Node> reduced = new HashSet<>();
 	
-	public boolean tryRuduce(Automaton automaton) {
-		return tryReduce(automaton.getStart(), this);
+	public boolean tryReduce(Automaton automaton) {
+		Node start = automaton.getStart();
+		return tryReduce(automaton, start, this, new ArrayList<>());
 	}
 	
-	protected boolean tryReduce(Node node, WalkGraphNode start) {
+	protected boolean tryReduce(Automaton automaton, Node curNode, WalkGraphNode start,
+			Iterable<RuleData> datas) {
 		boolean result = false;
-		if (node.isTermainal && !reduced.contains(node)) {
-			reduced.add(node);
+		if (curNode.isTermainal && !reduced.contains(curNode)) {
+			reduced.add(curNode);
 			result = true;
-			start.reduce();
+			List<RuleData> destDatas = automaton.getSequence(curNode, datas);
+			start.reduce(destDatas, this);
 		}
 		for (Entry<RuleData, WalkGraphNode> entry : edges.entrySet()) {
 			RuleData data = entry.getKey();
-			Node nextInAutomaton = node.getRule(data);
+			Node nextInAutomaton = curNode.getRule(data);
 			if (nextInAutomaton == null)
 				continue;
 			WalkGraphNode nextInGraph = entry.getValue();
-			result |= nextInGraph.tryReduce(nextInAutomaton, start);
+			result |= nextInGraph.tryReduce(automaton, nextInAutomaton, start,
+					new ConcatIterable<RuleData>(datas, data));
 		}
 		return result;
 	}
 	
-	private void reduce() {
+	private void reduce(List<RuleData> destDatas, WalkGraphNode endNode) {
+		WalkGraphNode current = this;
+		RuleData lastData = destDatas.get(destDatas.size() - 1);
+		for (RuleData data : destDatas) {
+			WalkGraphNode node = data == lastData ? endNode : new WalkGraphNode();
+			current.edges.put(data, node);
+			current = node;
+		}
+	}
+	
+	private class ConcatIterable<T> implements Iterable<T> {
+
+		private final Iterable<T> base;
+		private final T newElement;
+		
+		public ConcatIterable(Iterable<T> base, T newElement) {
+			this.base = base;
+			this.newElement = newElement;
+		}
+		
+		@Override
+		public Iterator<T> iterator() {
+			return new Iterator<T>() {
+
+				private final Iterator<T> baseIterator = base.iterator();
+				private boolean newElementUsed;
+				
+				@Override
+				public boolean hasNext() {
+					return baseIterator.hasNext() || !newElementUsed;
+				}
+
+				@Override
+				public T next() {
+					if (baseIterator.hasNext())
+						return baseIterator.next();
+					newElementUsed = true;
+					return newElement;
+				}
+			};
+		}
 		
 	}
 }
